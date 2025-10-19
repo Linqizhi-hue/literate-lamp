256150931fdc7d827747cf7bc455ee39a2ca736c
Linqizhi-hue <3310863697@qq.com>
2025-10-19 17:30:25 +0800

    test: Pass move command test cases

--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -10,6 +10,20 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose)
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
+    if (commands =="M") {
+        if (pose.heading=='E') {
+            pose.x+=1;
+        }
+        else if (pose.heading=='W') {
+            pose.x-=1;
+        }
+        else if (pose.heading=='N') {
+            pose.y+=1;
+        }
+        else if (pose.heading=='S') {
+            pose.y-=1;
+        }
+    }
 }
 Pose ExecutorImpl::Query() const noexcept
 {
