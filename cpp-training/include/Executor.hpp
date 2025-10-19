7dab8a83d24ab62c48286b70f3e689f6dd2409e8
Linqizhi-hue <3310863697@qq.com>
2025-10-19 16:31:54 +0800

    feat: Add test cases for initial and default poses - should_return_init_pose_when_without_command: Ensures the initial pose is returned in the absence of a command. - should_return_default_pose_when_without_init_and_command: Ensures the default pose is returned when there is no initial pose or command.

--- cpp-training/include/Executor.hpp
+++ cpp-training/include/Executor.hpp
@@ -10,7 +10,7 @@ struct Pose {
 class Executor
 {
 public:
-     static Executor* NewExecutor(const Pose& pose) noexcept;
+     static Executor* NewExecutor(const Pose& pose = {0,0,'N'}) noexcept;
 public:
     Executor(void) = default;
     virtual ~Executor(void) = default;
@@ -20,4 +20,5 @@ public:
     virtual void Execute(const std::string& command) noexcept = 0;
     virtual Pose Query(void) const noexcept = 0;
 };
+
 }
