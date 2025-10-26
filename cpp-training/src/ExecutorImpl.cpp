3698c564359dbf8fc38eea3585ac0fe0c1f9f6a6
Linqizhi-hue <3310863697@qq.com>
2025-10-26 15:23:04 +0800

    test: Pass turnleft and turnright command test cases

--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -24,6 +24,34 @@ void ExecutorImpl::Execute(const std::string& commands) noexcept
             pose.y-=1;
         }
     }
+    else if (commands == 'L') {
+        if (pose.heading == 'E') {
+            pose.heading = 'N';
+        } 
+        else if (pose.heading == 'N') {
+            pose.heading = 'W';
+        } 
+        else if (pose.heading == 'W') {
+            pose.heading = 'S';
+        } 
+        else if (pose.heading == 'S') {
+            pose.heading = 'E';
+        }
+    }
+    else if (commands == 'R') {
+        if (pose.heading == 'E') {
+            pose.heading = 'S';
+        } 
+        else if (pose.heading == 'S') {
+            pose.heading = 'W';
+        } 
+        else if (pose.heading == 'W') {
+            pose.heading = 'N';
+        } 
+        else if (pose.heading == 'N') {
+            pose.heading = 'E';
+        }
+    }
 }
 Pose ExecutorImpl::Query() const noexcept
 {
