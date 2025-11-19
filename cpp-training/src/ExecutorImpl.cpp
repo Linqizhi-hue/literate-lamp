8862161b47047041d4a5636b2773953d8a6f922f
Linqizhi-hue <3310863697@qq.com>
2025-11-20 01:27:00 +0800

    Introduce a singleton class

--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -2,9 +2,16 @@
 
 #include <memory>
 
+#include <unordered_map>
+
+#include "CmderFactory.hpp"
+
 #include "Command.hpp"
 
-#include <unordered_map>
+#include "Singleton.hpp"
+
+#include <algorithm>
+
 namespace adas
 {
 Executor* Executor::NewExecutor(const Pose& pose) noexcept
@@ -16,19 +23,8 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : poseHandler(pose)
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
-    const std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap{
-        {'M', MoveCommand()}, 
-        {'L', TurnLeftCommand()}, 
-        {'R', TurnRightCommand()},
-        {'F', FastCommand()}, 
-        {'B', ReverseCommand()},
-    };
-    for (const auto cmd : commands){
-        const auto it = cmderMap.find(cmd);
-        if (it != cmderMap.end()){
-            it->second(poseHandler);
-        }
-    }
+    const auto cmders = Singleton<CmderFactory>::Instance().GetCmders(commands);
+    std::for_each(cmders.begin(), cmders.end(), [this](const Cmder& cmder) noexcept { cmder(poseHandler); });
 }
 Pose ExecutorImpl::Query() const noexcept
 {
